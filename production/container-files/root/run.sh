2175e0d7ce19feefc35f846057298730d0b60099
Tobias Munk <schmunk@usrbin.de>
2014-12-23 18:14:15 +0100

    fixed quotes in ENV vars

--- production/container-files/root/run.sh
+++ production/container-files/root/run.sh
@@ -6,10 +6,10 @@ function setEnvironmentVariable() {
             echo "Environment variable '$1' not set."
             return
     fi
-    echo 'env[$1] = "$2"' >> /etc/php5/fpm/pool.d/www.conf
+    echo "env[$1] = \"$2\"" >> /etc/php5/fpm/pool.d/www.conf
 }
 
-# Grep for variables that look like docker set them (_PORT_)
+# Grep all ENV variables
 for _curVar in `env | awk -F = '{print $1}'`;do
     # awk has split them by the equals sign
     # Pass the name and value to our function
